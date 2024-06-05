e37aef98527f9c596464f3b38a70879b7d484722
יעל פרת <ylprt@h-MacBook-Pro-sl-yl.local>
2024-06-05 16:20:29 +0300

    cognito keys

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_d0kQYEbj3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'm8jr1gj4297dakc6nch3jj9rk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
